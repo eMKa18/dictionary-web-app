cc838fda1b41c05eff12ba4f46db619f133e892c
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-07-22 16:09:25 +0200

    Trying to make navbar

--- app/navbar/Navbar.tsx
+++ app/navbar/Navbar.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
+// import "./Navbar.css";
+
 const Navbar = () => {
   return (
-    <header>
-      <nav>
-        <Image
-          // can ommit public due to next js default config
-          src="images/logo.svg"
-          alt={"logo"}
-          width={50}
-          height={50}
-        ></Image>
+    <header className="ml-auto mr-auto border-solid border-2 border-amber-100">
+      <nav className="flex items-center">
+        <div className="float-left">
+          <Image
+            // can ommit public due to next js default config
+            src="images/logo.svg"
+            alt={"logo"}
+            width={50}
+            height={50}
+            className="mr-4"
+          ></Image>
+        </div>
+        <div className="flex flex-row items-center justify-end w-3/4 float-right">
+          <div>Font Drop Down</div>
+          <div>Dark Mode Switch</div>
+          <div>Dark/Light Mode Icon</div>
+        </div>
       </nav>
     </header>
   );
