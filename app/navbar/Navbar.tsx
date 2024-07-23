cfba837964c921cf10931b45c8b052a9847f5841
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-07-23 14:29:58 +0200

    Moving dark mode toggle to separate component, dark mode fix

--- app/navbar/Navbar.tsx
+++ app/navbar/Navbar.tsx
@@ -2,39 +2,14 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useTheme } from "next-themes";
 import "./Navbar.css";
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config";
 
-import { SVGProps } from "react";
-const MoonIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} {...props}>
-    <path
-      fill="none"
-      stroke={props.stroke || "#838383"}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={1.5}
-      d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
-    />
-  </svg>
-);
-// export { SvgComponent as ReactComponent };
-const fullConfig = resolveConfig(tailwindConfig);
-const activeColor = fullConfig.theme.colors.active;
+import { ModeSwitch } from "./darkmode/ModeSwitch";
 
 const Navbar = () => {
-  const { theme, setTheme } = useTheme();
-  const [isDarkMode, setIsDarkMode] = useState(false);
   const [selectedFont, setSelectedFont] = useState("Arial");
 
   const fonts = ["Arial", "Verdana", "Helvetica", "Times New Roman"];
-
-  const toggleDarkMode = () => {
-    setTheme(isDarkMode ? "light" : "dark");
-    setIsDarkMode(!isDarkMode);
-  };
 
   const handleFontChange = (event: {
     target: { value: React.SetStateAction<string> };
@@ -67,28 +42,7 @@ const Navbar = () => {
             ))}
           </select>
           <div className="border-r border-gray-500 mx-3 h-8"></div>
-          <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-            <input
-              type="checkbox"
-              name="toggle"
-              id="toggle"
-              className={`toggle-checkbox absolute block w-6 h-6 rounded-full appearance-none cursor-pointer transition-transform duration-200 ease-in-out ${isDarkMode ? " bg-active" : "border-inactive bg-inactive"}`}
-              checked={isDarkMode}
-              onChange={toggleDarkMode}
-            />
-            {/* This makes a background of toggle, trick is that there is no label text, instead we have the span with a dot which is representing a state of selection */}
-            <label
-              htmlFor="toggle"
-              className={`toggle-label block overflow-hidden w-10 h-6 rounded-full cursor-pointer ${isDarkMode ? "bg-active" : "bg-inactive"}`}
-            >
-              <span
-                className={`absolute top-1 left-1 w-4 h-4 rounded-full bg-white transition-transform duration-200 ease-in-out transform ${
-                  isDarkMode ? "translate-x-4" : "translate-x-0"
-                }`}
-              ></span>
-            </label>
-          </div>
-          {isDarkMode ? <MoonIcon stroke={activeColor} /> : <MoonIcon />}
+          <ModeSwitch />
         </div>
       </nav>
     </div>
---
+++ app/navbar/darkmode/ModeSwitch.tsx
@@ -0,0 +1,52 @@
+import { useTheme } from "next-themes";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../../tailwind.config";
+
+import { MoonIcon } from "./MoonIcon";
+import { useEffect, useState } from "react";
+
+const ModeSwitch = () => {
+  const defaultStrokeColor = "#838383";
+  const fullConfig = resolveConfig(tailwindConfig);
+  const activeColor = fullConfig.theme.colors.active;
+
+  const [strokeColor, setStrokeColor] = useState(defaultStrokeColor);
+
+  const { theme, setTheme } = useTheme();
+  const isDarkMode = () => theme === "dark";
+
+  useEffect(() => {
+    setStrokeColor(isDarkMode() ? activeColor : defaultStrokeColor);
+  }, [theme]);
+
+  const toggleDarkMode = () => {
+    setTheme(isDarkMode() ? "light" : "dark");
+  };
+
+  return (
+    <>
+      <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+        <input
+          type="checkbox"
+          name="toggle"
+          id="toggle"
+          className={`toggle-checkbox absolute block w-6 h-6 rounded-full appearance-none cursor-pointer transition-transform duration-200 ease-in-out dark:bg-active bg-inactive`}
+          checked={isDarkMode()}
+          onChange={toggleDarkMode}
+        />
+        {/* This makes a background of toggle, trick is that there is no label text, instead we have the span with a dot which is representing a state of selection */}
+        <label
+          htmlFor="toggle"
+          className={`toggle-label block overflow-hidden w-10 h-6 rounded-full cursor-pointer dark:bg-active bg-inactive`}
+        >
+          <span
+            className={`absolute top-1 left-1 w-4 h-4 rounded-full bg-white transition-transform duration-200 ease-in-out transform dark:translate-x-4 translate-x-0`}
+          ></span>
+        </label>
+      </div>
+      <MoonIcon stroke={strokeColor} />
+    </>
+  );
+};
+
+export { ModeSwitch };
